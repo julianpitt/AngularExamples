7e6a47e2f6da99d1eedd7447a9ffbd5b6c7a44bc
Julian Pittas <julian.pittas@gmail.com>
2016-11-27 16:33:37 +1100

    Removed controller-dashboard for it's own repo

--- controller-dashboard/app/routers/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Page2 } from "../components/page2.component";
-import { Page1 } from "../components/page1.component";
-
-export const AppRoutes: any = [
-    { path: "page2", component: Page2 },
-    { path: "", component: Page1 }
-];
